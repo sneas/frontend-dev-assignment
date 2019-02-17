2186f3a4704b07ec38a86e8b7b5ce53d49a11f6d
Dima Snisarenko <snisarenkodima@gmail.com>
2019-02-17 19:27:06 +0100

    Show suggestion box

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,84 @@
 import React, { Component } from 'react';
 import Search from './Search';
 import * as api from '../utils/api';
+import SuggestionBox from './SuggestionBox';
 
 class App extends Component {
   state = {
     query: '',
+    suggestions: [],
+    suggestionIndex: -1,
   }
 
   handleQueryChange = (query) => {
     this.setState({query});
-    api.search(query).then(res => console.log(res));
+
+    if (query.length < 2) {
+      this.setState({suggestions: []});
+      return;
+    }
+
+    api.search(query).then(res => {
+      this.setState({
+        suggestions: res.suggestions.map(suggestion => suggestion.searchterm)
+      });
+    });
+  }
+
+  handleKeyDown = (e) => {
+    const code = e.nativeEvent.keyCode;
+
+    switch (code) {
+      case 27:
+        this.hideSuggestion();
+        break;
+      case 40:
+        this.increaseSuggestionIndex();
+        break;
+      case 38:
+        this.decreaseSuggestionIndex();
+    }
+  }
+
+  handleKeyUp = (e) => {
+
+  }
+
+  hideSuggestion() {
+    this.setState({suggestions: [], suggestionIndex: -1});
+  }
+
+  increaseSuggestionIndex() {
+    this.setState(prev => ({
+      suggestionIndex: prev.suggestionIndex + 1 < prev.suggestions.length ? prev.suggestionIndex + 1 : -1
+    }))
+  }
+
+  decreaseSuggestionIndex() {
+    this.setState(prev => ({
+      suggestionIndex: prev.suggestionIndex === 0 ? prev.suggestions.length - 1 : prev.suggestionIndex - 1
+    }))
   }
 
   render() {
+    let searchValue = this.state.query;
+
+    if (this.state.suggestionIndex > -1) {
+      searchValue = this.state.suggestions[this.state.suggestionIndex];
+    }
+
     return (
       <div className="container pt-1">
-        <Search value={this.state.query} onChange={this.handleQueryChange} />
+        <Search value={searchValue}
+                onChange={this.handleQueryChange}
+                onKeyDown={this.handleKeyDown}
+                onKeyUp={this.handleKeyUp}
+        />
+        {this.state.suggestions.length > 0 && (
+            <SuggestionBox list={this.state.suggestions}
+                           index={this.state.suggestionIndex}
+            />
+        )}
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -8,10 +8,20 @@ class Search extends Component {
   static propTypes = {
     value: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
+    onKeyDown: PropTypes.func,
+    onKeyUp: PropTypes.func,
   }
 
   handleQueryChange = (e) => {
     this.props.onChange(e.target.value);
+  }
+
+  handleKeyDown = (e) => {
+    this.props.onKeyDown && this.props.onKeyDown(e);
+  }
+
+  handleKeyUp = (e) => {
+    this.props.onKeyUp && this.props.onKeyUp(e);
   }
 
   clear = () => {
@@ -21,7 +31,13 @@ class Search extends Component {
   render() {
     return (
         <label className="search">
-          <input type="text" className="search__input" value={this.props.value} onChange={this.handleQueryChange} />
+          <input type="text"
+                 className="search__input"
+                 value={this.props.value}
+                 onChange={this.handleQueryChange}
+                 onKeyDown={this.handleKeyDown}
+                 onKeyUp={this.handleKeyUp}
+          />
           {this.props.value !== '' && (
               <FontAwesomeIcon className="search__icon search__icon--clickable" icon={faTimesCircle} onClick={this.clear} />
           )}
